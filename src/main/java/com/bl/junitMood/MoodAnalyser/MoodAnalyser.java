3bd2c2fc76f8efd234a527d32432a189cf8173ff
Jagriti <jagriti.agni@gmail.com>
2020-09-30 18:12:56 +0530

    [Jagriti]ADDED: test case to analyse sad or happy

--- src/main/java/com/bl/junitMood/MoodAnalyser/MoodAnalyser.java
+++ src/main/java/com/bl/junitMood/MoodAnalyser/MoodAnalyser.java
@@ -1,13 +1,18 @@
 package com.bl.junitMood.MoodAnalyser;
 
-/**
- * Hello world!
- *
- */
 public class MoodAnalyser 
 {
 	public boolean printWelcome() {
 		System.out.println("Welcome to the mood analyser system");
 		return true;
 	}
+	
+	public String analyseMood(String mood) {
+		if (mood.contains("sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
+	
 }
--- src/test/java/com/bl/junitMood/MoodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/bl/junitMood/MoodAnalyser/MoodAnalyserTest.java
@@ -6,8 +6,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.bl.junitTwo.JunitPracticeTwoMaven.MoodAnalyser;
-
 public class MoodAnalyserTest 
 {
 	MoodAnalyser moodAnalyser = null;
@@ -23,4 +21,22 @@ public class MoodAnalyserTest
         boolean welcome = moodAnalyser.printWelcome();
         Assert.assertEquals(true, welcome);
     }
+	
+	@Test
+	public void givenMessage_whenSad_shouldReturnSad() {
+		String mood = moodAnalyser.analyseMood("I am in sad mood.");
+		Assert.assertEquals("SAD", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenHappy_shouldReturnHappy() {
+		String mood = moodAnalyser.analyseMood("This is a happy message");
+		Assert.assertEquals("HAPPY", mood);
+	}
+	
+	@Test
+	public void givenMessage_whenAnymood_shouldReturnHappy() {
+		String mood = moodAnalyser.analyseMood("I am in Any Mood");
+		Assert.assertEquals("HAPPY", mood);
+	}
 }
